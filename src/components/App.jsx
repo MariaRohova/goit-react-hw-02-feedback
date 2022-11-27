495a65acc09d49e8a1c3f83a750df52a4a56e5fb
Maria Rohova <m.rohovaya@gmail.com>
2022-11-27 23:51:00 +0200

    update Notification

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Section from './Section/Section';
 import FeedbackOptions from "./FeedbackOptions/FeedbackOptions";
 import Statistics from "./Statistics/Statistics";
-import Notification from "./Statistics/Notification";
+import Notification from "./Notification/Notification";
 
 class App extends Component {
   state = {
